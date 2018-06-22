c0966bb79b6376837e975f6beef03ff4d90fc09d
Eduardo Ogasawara <eogasawara@eic.cefet-rj.br>
2018-06-22 20:23:50 -0300

    Update

---
+++ myFeatures.R
@@ -0,0 +1,113 @@
+loadlibrary <- function(x)
+{
+  if (!require(x,character.only = TRUE))
+  {
+    install.packages(x, repos='http://cran.fiocruz.br', dep=TRUE)
+    loadlibrary(x)
+  }
+}
+
+loadlibrary("glmnet")
+loadlibrary("leaps")
+loadlibrary("FSelector")
+
+
+# FEATURE SELECTION
+
+
+#Lasso
+
+fs.lasso <- function(data, class)
+{
+  data = data.frame(data)
+  dataorg <- data
+  if (!is.numeric(data[,class]))
+    data[,class] =  as.numeric(data[,class])
+  nums = unlist(lapply(data, is.numeric))
+  data = data[ , nums]
+  predictors_name  = setdiff(colnames(data), class)
+  predictors = as.matrix(data[,predictors_name])
+  predictand = data[,class]
+  grid = 10^ seq (10,-2, length = 100)
+  cv.out = cv.glmnet (predictors, predictand, alpha = 1)
+  bestlam = cv.out$lambda.min
+  out = glmnet(predictors, predictand, alpha = 1, lambda = grid)
+  lasso.coef = predict (out,type = "coefficients", s = bestlam)
+  l = lasso.coef[(lasso.coef[,1]) != 0,0]
+  vec = rownames(l)[-1]
+  data = dataorg[,c(vec, class)]
+  return (list(data=data, features=vec))
+}
+
+
+# Forward Stepwise Selection
+
+fs.fss <- function(data, class)
+{
+  data = data.frame(data)
+  dataorg <- data
+  if (!is.numeric(data[,class]))
+    data[,class] =  as.numeric(data[,class])
+  nums = unlist(lapply(data, is.numeric))
+  data = data[ , nums]
+  
+  predictors_name  = setdiff(colnames(data), class)
+  predictors = as.matrix(data[,predictors_name])
+  predictand = data[,class]
+  
+  regfit.fwd = regsubsets(predictors, predictand, nvmax=ncol(data)-1, method="forward")  
+  summary(regfit.fwd)
+  reg.summaryfwd = summary(regfit.fwd)
+  b1 = which.max(reg.summaryfwd$adjr2)
+  t = coef(regfit.fwd,b1)
+  vec = names(t)[-1]
+  data = dataorg[,c(vec, class)]
+  return (list(data=data, features=vec))
+}
+
+# Correlation-based Feature Selection (CFS)
+
+fs.cfs <- function(data, class)
+{
+  class_formula = formula(paste(class, "  ~ ."))
+  vec = cfs(class_formula, data)
+  data = data[,c(vec, class)]
+  return (list(data=data, features=vec))
+}
+
+# Information Gain
+
+fs.ig <- function(data, class)
+{
+  class_formula = formula(paste(class, "  ~ ."))
+  weights = information.gain(class_formula, data)
+  
+  tab=data.frame(weights)
+  tab=orderBy(~-attr_importance, data=tab)
+  tab$i=row(tab)
+  tab$import_acum=cumsum(tab$attr_importance)
+  res = curvature.min(tab$i, tab$import_acum)
+  tab = tab[tab$import_acum <= res$y,]
+  vec = rownames(tab)
+  data = data[,c(vec, class)]
+  return (list(data=data, features=vec))
+}
+
+# Relief
+
+fs.relief <- function(data, class)
+{
+  class_formula = formula(paste(class, "  ~ ."))
+  weights = relief(class_formula, data)
+  
+  tab=data.frame(weights)
+  tab=orderBy(~-attr_importance, data=tab)
+  tab$i=row(tab)
+  tab$import_acum=cumsum(tab$attr_importance)
+  res = curvature.min(tab$i, tab$import_acum)
+  tab = tab[tab$import_acum <= res$y,]
+  vec = rownames(tab)
+  data = data[,c(vec, class)]
+  return (list(data=data, features=vec))
+}
+
--- myPreprocessing.R
+++ myPreprocessing.R
@@ -1,4 +1,4 @@
-loadlibrary <- function(x)
+loadlibrary <- function(x) 
 {
   if (!require(x,character.only = TRUE))
   {
@@ -9,11 +9,6 @@ loadlibrary <- function(x)
 
 loadlibrary("caret")
 loadlibrary("MASS")
-loadlibrary("glmnet")
-loadlibrary("leaps")
-loadlibrary("FSelector")
-loadlibrary("doBy")
-loadlibrary("DMwR")
 
 # samples
 
@@ -166,107 +161,6 @@ normalize.zscore <- function(data, norm.set=NULL, nmean=0, nsd=1)
     data[,i] = ((data[,i] - zscore["mean", i]) / zscore["sd", i]) * zscore["nsd", i] + zscore["nmean", i]
   return (list(data=data, norm.set=zscore))
 }
-
-
-# FEATURE SELECTION
-
-
-#Lasso
-
-fs.lasso <- function(data, class)
-{
-  data = data.frame(data)
-  dataorg <- data
-  if (!is.numeric(data[,class]))
-    data[,class] =  as.numeric(data[,class])
-  nums = unlist(lapply(data, is.numeric))
-  data = data[ , nums]
-  predictors_name  = setdiff(colnames(data), class)
-  predictors = as.matrix(data[,predictors_name])
-  predictand = data[,class]
-  grid = 10^ seq (10,-2, length = 100)
-  cv.out = cv.glmnet (predictors, predictand, alpha = 1)
-  bestlam = cv.out$lambda.min
-  out = glmnet(predictors, predictand, alpha = 1, lambda = grid)
-  lasso.coef = predict (out,type = "coefficients", s = bestlam)
-  l = lasso.coef[(lasso.coef[,1]) != 0,0]
-  vec = rownames(l)[-1]
-  data = dataorg[,c(vec, class)]
-  return (list(data=data, features=vec))
-}
-
-
-# Forward Stepwise Selection
-
-fs.fss <- function(data, class)
-{
-  data = data.frame(data)
-  dataorg <- data
-  if (!is.numeric(data[,class]))
-    data[,class] =  as.numeric(data[,class])
-  nums = unlist(lapply(data, is.numeric))
-  data = data[ , nums]
-  
-  predictors_name  = setdiff(colnames(data), class)
-  predictors = as.matrix(data[,predictors_name])
-  predictand = data[,class]
-  
-  regfit.fwd = regsubsets(predictors, predictand, nvmax=ncol(data)-1, method="forward")  
-  summary(regfit.fwd)
-  reg.summaryfwd = summary(regfit.fwd)
-  b1 = which.max(reg.summaryfwd$adjr2)
-  t = coef(regfit.fwd,b1)
-  vec = names(t)[-1]
-  data = dataorg[,c(vec, class)]
-  return (list(data=data, features=vec))
-}
-
-# Correlation-based Feature Selection (CFS)
-
-fs.cfs <- function(data, class)
-{
-  class_formula = formula(paste(class, "  ~ ."))
-  vec = cfs(class_formula, data)
-  data = data[,c(vec, class)]
-  return (list(data=data, features=vec))
-}
-
-# Information Gain
-
-fs.ig <- function(data, class)
-{
-  class_formula = formula(paste(class, "  ~ ."))
-  weights = information.gain(class_formula, data)
-  
-  tab=data.frame(weights)
-  tab=orderBy(~-attr_importance, data=tab)
-  tab$i=row(tab)
-  tab$import_acum=cumsum(tab$attr_importance)
-  res = curvature.min(tab$i, tab$import_acum)
-  tab = tab[tab$import_acum <= res$y,]
-  vec = rownames(tab)
-  data = data[,c(vec, class)]
-  return (list(data=data, features=vec))
-}
-
-# Relief
-
-fs.relief <- function(data, class)
-{
-  class_formula = formula(paste(class, "  ~ ."))
-  weights = relief(class_formula, data)
-  
-  tab=data.frame(weights)
-  tab=orderBy(~-attr_importance, data=tab)
-  tab$i=row(tab)
-  tab$import_acum=cumsum(tab$attr_importance)
-  res = curvature.min(tab$i, tab$import_acum)
-  tab = tab[tab$import_acum <= res$y,]
-  vec = rownames(tab)
-  data = data[,c(vec, class)]
-  return (list(data=data, features=vec))
-}
-
 
 # Data Transformation
 
