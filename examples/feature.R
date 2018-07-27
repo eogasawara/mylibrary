e9aa51a097b313cb25b22774f488ec4fda0edece
Eduardo Ogasawara <eogasawara@eic.cefet-rj.br>
2018-07-27 08:44:12 -0300

    Feature

---
+++ examples/feature.R
@@ -0,0 +1,24 @@
+source("https://raw.githubusercontent.com/eogasawara/mylibrary/master/myPreprocessing.R")
+source("https://raw.githubusercontent.com/eogasawara/mylibrary/master/myFeatures.R")
+
+data(iris)
+
+# FEATURE SELECTION
+
+myfeature <- fs.lasso(iris, "Species")
+print(myfeature$features)
+
+myfeature <- fs.fss(iris, "Species")
+print(myfeature$features)
+
+myfeature <- fs.cfs(iris, "Species")
+print(myfeature$features)
+
+options(repr.plot.width=3, repr.plot.height=3)
+myfeature <- fs.ig(iris, "Species")
+print(myfeature$features)
+
+options(repr.plot.width=3, repr.plot.height=3)
+myfeature <- fs.relief(iris, "Species")
+print(myfeature$features)
+
