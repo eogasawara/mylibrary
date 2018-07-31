2f32ef9eed06454810d2dd83ef78d5e926e9ab6a
Eduardo Ogasawara <eogasawara@eic.cefet-rj.br>
2018-07-31 01:44:41 -0300

    myprediction

--- myPrediction.R
+++ myPrediction.R
@@ -15,201 +15,176 @@ loadlibrary("e1071")
 loadlibrary("class")
 loadlibrary("randomForest")
 loadlibrary("Matrix")
+loadlibrary("ROCR")
+
+# CLASSIFICATION: General Test Class
+class_test <- function(model, test, clabel, predtype=NULL) {
+  predictors_name  = setdiff(colnames(test), clabel)
+  test_predictors = test[,predictors_name] 
+  test_predictand_clabel = decodeClassLabels(test[,clabel])
+  predictions = predict(model, test_predictors, type=predtype)
+  classification = data.frame(predictions,test_predictand_clabel)
+  confTest = confusionMatrix(test_predictand_clabel,predictions)
+  return (list(model=model, predictions=classification, cmTest=confTest)) 
+}
 
 
 # CLASSIFICATION: MLP usando NNET
 
-class_mlp_nnet <- function(data, test, clabel, neurons=3, decay=0.01, iterations=5000)
+class_mlp_nnet <- function(data, clabel, neurons=3, decay=0.01, iterations=5000)
 {
   predictors_name  = setdiff(colnames(data), clabel)
-
   data_predictors = data[,predictors_name] 
   data_predictand_clabel = decodeClassLabels(data[,clabel])
-  
-  test_predictors = test[,predictors_name] 
-  test_predictand_clabel = decodeClassLabels(test[,clabel])
-  
+  data[,clabel] = as.factor(data[,clabel])
+  class_formula = formula(paste(clabel, "  ~ ."))  
+
   model = nnet(data_predictors, data_predictand_clabel, size=neurons, decay=decay, maxit=iterations)
-  predictions_train = predict(model, data_predictors, type="raw")  
-  predictions = predict(model, test_predictors, type="raw")
+  predtype = "raw"
+  predictions = predict(model, data_predictors, type=predtype)  
   
-  classification = data.frame(predictions,test_predictand_clabel)
-  confTrain = confusionMatrix(data_predictand_clabel,predictions_train)
-  confTest = confusionMatrix(test_predictand_clabel,predictions)
-  return (list(classification, confTrain, confTest, model)) 
+  classification = data.frame(predictions,data_predictand_clabel)
+  confTrain = confusionMatrix(data_predictand_clabel,predictions)
+  return (list(model=model, predtype=predtype, predictions=classification, cmTrain=confTrain)) 
 }
 
 # CLASSIFICATION: SVM KERNEL RBF usando kernlab
 
-class_svm_rbf <- function(data, test, clabel, C=10, sigma=0.1)
+class_svm_rbf <- function(data, clabel, C=10, sigma=0.1)
 {
   predictors_name  = setdiff(colnames(data), clabel)
-  
   data_predictors = data[,predictors_name] 
   data_predictand_clabel = decodeClassLabels(data[,clabel])
   data[,clabel] = as.factor(data[,clabel])
-
-  test_predictors = test[,predictors_name] 
-  test_predictand_clabel = decodeClassLabels(test[,clabel])
-  
   class_formula = formula(paste(clabel, "  ~ ."))  
+
   rbf = rbfdot(sigma=sigma)
-
   model = ksvm(class_formula,data=data,type="C-bsvc",kernel=rbf,C=C,prob.model=TRUE)
-  predictions_train = predict(model, data_predictors, type="probabilities")  
-  predictions = predict(model, test_predictors, type="probabilities")
-  
-  classification = data.frame(predictions,test_predictand_clabel)
-  confTrain = confusionMatrix(data_predictand_clabel,predictions_train)
-  confTest = confusionMatrix(test_predictand_clabel,predictions)
-  return (list(classification, confTrain, confTest, model)) 
-} 
+  predtype = "probabilities"
+  predictions = predict(model, data_predictors, type=predtype)  
 
-class_svm_poly <- function(data, test, clabel, C=10, degree = 1, scale = 1, offset = 1)
+  classification = data.frame(predictions,data_predictand_clabel)
+  confTrain = confusionMatrix(data_predictand_clabel,predictions)
+  return (list(model=model, predtype=predtype, predictions=classification, cmTrain=confTrain)) 
+}
+
+class_svm_poly <- function(data, clabel, C=10, degree = 1, scale = 1, offset = 1)
 {
   predictors_name  = setdiff(colnames(data), clabel)
-  
   data_predictors = data[,predictors_name] 
   data_predictand_clabel = decodeClassLabels(data[,clabel])
   data[,clabel] = as.factor(data[,clabel])
-
-  test_predictors = test[,predictors_name] 
-  test_predictand_clabel = decodeClassLabels(test[,clabel])
-  
   class_formula = formula(paste(clabel, "  ~ ."))  
+  
   polyk = polydot(degree = degree, scale = scale, offset = offset)
-
   model = ksvm(class_formula,data=data,type="C-bsvc",kernel=polyk, C=C, prob.model=TRUE)
-  predictions_train = predict(model, data_predictors, type="probabilities")  
-  predictions = predict(model, test_predictors, type="probabilities")
+  predtype = "probabilities"
+  predictions = predict(model, data_predictors, type=predtype)  
   
-  classification = data.frame(predictions,test_predictand_clabel)
-  confTrain = confusionMatrix(data_predictand_clabel,predictions_train)
-  confTest = confusionMatrix(test_predictand_clabel,predictions)
-  return (list(classification, confTrain, confTest, model)) 
+  classification = data.frame(predictions,data_predictand_clabel)
+  confTrain = confusionMatrix(data_predictand_clabel,predictions)
+  return (list(model=model, predtype=predtype, predictions=classification, cmTrain=confTrain)) 
 } 
 
-class_svm_sigmoid <- function(data, test, clabel, C=10, scale = 1, offset = 1)
+class_svm_sigmoid <- function(data, clabel, C=10, scale = 1, offset = 1)
 {
   predictors_name  = setdiff(colnames(data), clabel)
-  
   data_predictors = data[,predictors_name] 
   data_predictand_clabel = decodeClassLabels(data[,clabel])
   data[,clabel] = as.factor(data[,clabel])
-  
-  test_predictors = test[,predictors_name] 
-  test_predictand_clabel = decodeClassLabels(test[,clabel])
-  
   class_formula = formula(paste(clabel, "  ~ ."))  
+  
   tanhk = tanhdot(scale = scale, offset = offset)
-  
   model = ksvm(class_formula,data=data,type="C-bsvc",kernel=tanhk, C=C, prob.model=TRUE)
-  predictions_train = predict(model, data_predictors, type="probabilities")  
-  predictions = predict(model, test_predictors, type="probabilities")
+  predtype = "probabilities"
+  predictions = predict(model, data_predictors, type=predtype)  
   
-  classification = data.frame(predictions,test_predictand_clabel)
-  confTrain = confusionMatrix(data_predictand_clabel,predictions_train)
-  confTest = confusionMatrix(test_predictand_clabel,predictions)
-  return (list(classification, confTrain, confTest, model)) 
+  classification = data.frame(predictions,data_predictand_clabel)
+  confTrain = confusionMatrix(data_predictand_clabel,predictions)
+  return (list(model=model, predtype=predtype, predictions=classification, cmTrain=confTrain)) 
 } 
-
 
 # CLASSIFICATION: MPL usando RSNNS
 
-class_mlp_RSNNS <- function(data, test, clabel, neurons=3, iterations=5000)
+class_mlp_RSNNS <- function(data, clabel, neurons=3, iterations=5000)
 {
   predictors_name  = setdiff(colnames(data), clabel)
-  
   data_predictors = data[,predictors_name] 
   data_predictand_clabel = decodeClassLabels(data[,clabel])
-  
-  test_predictors = test[,predictors_name] 
-  test_predictand_clabel = decodeClassLabels(test[,clabel])
+  data[,clabel] = as.factor(data[,clabel])
+  class_formula = formula(paste(clabel, "  ~ ."))  
   
   model = mlp(data_predictors, data_predictand_clabel, size=neurons, learnFuncParams=c(0.1), maxit=iterations)
-  predictions_train = predict(model, data_predictors)  
-  predictions = predict(model, test_predictors)
+  predtype = NULL
+  predictions = predict(model, data_predictors)  
   
-  classification = data.frame(predictions,test_predictand_clabel)
-  confTrain = confusionMatrix(data_predictand_clabel,predictions_train)
-  confTest = confusionMatrix(test_predictand_clabel,predictions)
-  return (list(classification, confTrain, confTest, model)) 
+  classification = data.frame(predictions,data_predictand_clabel)
+  confTrain = confusionMatrix(data_predictand_clabel,predictions)
+  return (list(model=model, predtype=predtype, predictions=classification, cmTrain=confTrain)) 
 }
 
-# CLASSIFICATION: RBF usando RSNNS
+# CLASSIFICATION: RBF usando RSNNSz
 
-class_rbf_RSNNS <- function(data, test, clabel, neurons=40, iterations=5000)
+class_rbf_RSNNS <- function(data, clabel, neurons=40, iterations=5000)
 {
   predictors_name  = setdiff(colnames(data), clabel)
-  
   data_predictors = data[,predictors_name] 
   data_predictand_clabel = decodeClassLabels(data[,clabel])
-  
-  test_predictors = test[,predictors_name] 
-  test_predictand_clabel = decodeClassLabels(test[,clabel])
+  data[,clabel] = as.factor(data[,clabel])
+  class_formula = formula(paste(clabel, "  ~ ."))  
   
   model = rbf(data_predictors, data_predictand_clabel, size=neurons, maxit=iterations,
-               initFuncParams=c(0, 1, 0, 0.01, 0.01),
-               learnFuncParams=c(1e-8, 0, 1e-8, 0.1, 0.8), linOut=TRUE)  
-  predictions_train = predict(model, data_predictors)  
-  predictions = predict(model, test_predictors)
+              initFuncParams=c(0, 1, 0, 0.01, 0.01),
+              learnFuncParams=c(1e-8, 0, 1e-8, 0.1, 0.8), linOut=TRUE)  
+  predtype = NULL
+  predictions = predict(model, data_predictors)  
   
-  classification = data.frame(predictions,test_predictand_clabel)
-  confTrain = confusionMatrix(data_predictand_clabel,predictions_train)
-  confTest = confusionMatrix(test_predictand_clabel,predictions)
-  return (list(classification, confTrain, confTest, model)) 
+  classification = data.frame(predictions,data_predictand_clabel)
+  confTrain = confusionMatrix(data_predictand_clabel,predictions)
+  return (list(model=model, predtype=predtype, predictions=classification, cmTrain=confTrain)) 
 }
 
 # CLASSIFICATION: NaiveBayes
 
-class_naiveBayes <- function(data, test, clabel)
+class_naiveBayes <- function(data, clabel)
 {
   predictors_name  = setdiff(colnames(data), clabel)
-  
   data_predictors = data[,predictors_name] 
   data_predictand_clabel = decodeClassLabels(data[,clabel])
-
-  test_predictors = test[,predictors_name] 
-  test_predictand_clabel = decodeClassLabels(test[,clabel])
-  
+  data[,clabel] = as.factor(data[,clabel])
   class_formula = formula(paste(clabel, "  ~ ."))  
   
   model = naiveBayes(class_formula, data, laplace=0)
-  predictions_train = predict(model, data_predictors, type="raw")  
-  predictions = predict(model, test_predictors, type="raw") 
-
-  classification = data.frame(predictions,test_predictand_clabel)
-  confTrain = confusionMatrix(data_predictand_clabel,predictions_train)
-  confTest = confusionMatrix(test_predictand_clabel,predictions)
-  return (list(classification, confTrain, confTest, model))  
+  predtype = "raw"
+  predictions = predict(model, data_predictors, type=predtype)  
+  
+  classification = data.frame(predictions,data_predictand_clabel)
+  confTrain = confusionMatrix(data_predictand_clabel,predictions)
+  return (list(model=model, predtype=predtype, predictions=classification, cmTrain=confTrain)) 
 }
 
 # CLASSIFICATION: Random Forest
 
-class_randomForest <- function(data, test, clabel, ntree = 100)
+class_randomForest <- function(data, clabel, ntree = 100)
 {
   predictors_name  = setdiff(colnames(data), clabel)
-  data_clabel = as.factor(data[,clabel])
-  
   data_predictors = data[,predictors_name] 
   data_predictand_clabel = decodeClassLabels(data[,clabel])
-
-  test_predictors = test[,predictors_name] 
-  test_predictand_clabel = decodeClassLabels(test[,clabel])
+  data[,clabel] = as.factor(data[,clabel])
+  class_formula = formula(paste(clabel, "  ~ ."))  
   
-  model = randomForest(data_predictors, data_clabel, ntree=ntree)
-  predictions_train = predict(model, data_predictors, type="prob")  
-  predictions = predict(model, test_predictors, type="prob")  
+  model = randomForest(data_predictors, data[,clabel], ntree=ntree)
+  predtype = "prob"
+  predictions = predict(model, data_predictors, type=predtype)  
   
-  classification = data.frame(predictions,test_predictand_clabel)
-  confTrain = confusionMatrix(data_predictand_clabel,predictions_train)
-  confTest = confusionMatrix(test_predictand_clabel,predictions)
-  return (list(classification, confTrain, confTest, model))  
+  classification = data.frame(predictions,data_predictand_clabel)
+  confTrain = confusionMatrix(data_predictand_clabel,predictions)
+  return (list(model=model, predtype=predtype, predictions=classification, cmTrain=confTrain))   
 }
 
 # CLASSIFICATION: KNN
 
-class_knn <- function(data, test, clabel, k = 10)
+knn_predict <- function(data, test, clabel, k = 10)
 {
   predictors_name  = setdiff(colnames(data), clabel)
   
@@ -223,22 +198,22 @@ class_knn <- function(data, test, clabel, k = 10)
   
   predictions_train = knn(data_predictors, data_predictors, data_clabel, k=k, prob=TRUE)
   predictions = knn(data_predictors, test_predictors, data_clabel, k=k, prob=TRUE)
-
+  
   predictions_train = decodeClassLabels(predictions_train)
   predictions = decodeClassLabels(predictions)
-
+  
   classification = data.frame(predictions, test_predictand_clabel)
   confTrain = confusionMatrix(data_predictand_clabel, predictions_train)
   confTest = confusionMatrix(test_predictand_clabel, predictions)
-  return (list(classification, confTrain, confTest, NULL))  
+  return (list(predictions=classification, cmTrain=confTrain, cmTest=confTest))  
 }
 
 # CLASSIFICATION: R0
 
-class_R0 <- function(data, test, clabel)
+R0_predict <- function(data, test, clabel)
 {
   predictors_name  = setdiff(colnames(data), clabel)
-
+  
   data_predictors = data[,predictors_name] 
   data_predictand_clabel = decodeClassLabels(data[,clabel])
   
@@ -258,12 +233,11 @@ class_R0 <- function(data, test, clabel)
     return(result)
   }
   
-  model = NULL
   predictions_train = r0_matrix(data_predictand_clabel, col)
   predictions = r0_matrix(test_predictand_clabel, col)
   
   classification = data.frame(predictions,test_predictand_clabel)
   confTrain = confusionMatrix(data_predictand_clabel,predictions_train)
   confTest = confusionMatrix(test_predictand_clabel,predictions)
-  return (list(classification, confTrain, confTest, model))  
+  return (list(predictions=classification, cmTrain=confTrain, cmTest=confTest))  
 }
