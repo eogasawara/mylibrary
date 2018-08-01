7bf56814f16c8a8eec9bcdad31bdca0507f3e9d3
Eduardo Ogasawara <eogasawara@eic.cefet-rj.br>
2018-08-01 12:04:03 -0300

    myclustering

--- myclustering.R
+++ myclustering.R
@@ -18,13 +18,30 @@ clust_kmeans <- function(data, clabel, k)
   entropy <- c(1:(length(l)+1))
   entropy[1] = (Entropy(table(data[,clabel]), base=exp(1)))
   ctable = table(data[,clabel])
+  
   for (i in 1:length(l)) {
     x <- factor(l[[i]], levels(data[,clabel]))
     ctable = rbind(ctable, table(x))
     entropy[i+1] = Entropy(table(x), base=exp(1))
-  }  
-  
+  } 
   return (list(data=data, cluster=clu, table=ctable, entropy=entropy, plot=plot(clu, data=data_predictors))) 
+}
+
+clust_kmeans_best <- function(data, maxk=20) {
+  x = c(1:maxk)
+  y = rep(0,maxk)
+  k = 1
+  for (k in 1:maxk) {
+    clu = kmeans(x = data, centers = k)
+    for (i in 1:k) {
+      mdata = data
+      mdata$cluster = clu$cluster
+      mdata = filter(mdata, cluster == i)
+      mdata$cluster = NULL
+      y[k] <- y[k] + sum(sqrt(rowSums(mdata - clu$centers[i,]) ^ 2))
+    }
+  }
+  return (list(x=x, y=y))
 }
 
 clust_dbscan <- function(data, clabel, eps, MinPts)
