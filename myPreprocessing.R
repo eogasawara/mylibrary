ffbb3a52d0aaa9c3652b1b43e7dada178f2f8b69
Eduardo Ogasawara <eogasawara@eic.cefet-rj.br>
2019-01-09 18:27:34 -0200

    Update myPreprocessing.R

--- myPreprocessing.R
+++ myPreprocessing.R
@@ -121,49 +121,65 @@ curvature.max <- function(x, y, df=3, do_plot=TRUE) {
 
 # min-max normalization
 
-normalize.minmax <- function(data, norm.set=NULL)
-{
+normalize.minmax <- function(data, norm.set=NULL){
   data = data.frame(data)
-  nums = unlist(lapply(data, is.numeric))
-  data = data[ , nums]
-  
   if(is.null(norm.set))
   {
-    minmax = data.frame(t(sapply(data, max, na.rm=TRUE)))
-    minmax = rbind(minmax, t(sapply(data, min, na.rm=TRUE)))
+    minmax = data.frame(t(ifelse(sapply(data, is.numeric), 1, 0)))
+    minmax = rbind(minmax, rep(NA, ncol(minmax)))
+    minmax = rbind(minmax, rep(NA, ncol(minmax)))
     colnames(minmax) = colnames(data)    
-    rownames(minmax) = c("max", "min")
+    rownames(minmax) = c("numeric", "max", "min")
+    for (j in colnames(minmax)[minmax["numeric",]==1]) {
+      minmax["min",j] <- min(data[,j], na.rm=TRUE)
+      minmax["max",j] <- max(data[,j], na.rm=TRUE)
+    }
   }
   else {
     minmax = norm.set
   }
-  for (i in 1:ncol(data))
-    data[,i] = (data[,i] - minmax["min", i]) / (minmax["max", i] - minmax["min", i])
+  for (j in colnames(minmax)[minmax["numeric",]==1]) {
+    if ((minmax["max", j] != minmax["min", j])) {
+      data[,j] = (data[,j] - minmax["min", j]) / (minmax["max", j] - minmax["min", j])
+    }
+    else {
+      data[,j] = 0
+    }
+  }
   return (list(data=data, norm.set=minmax))
 }
 
-
 # z-score normalization
 
-normalize.zscore <- function(data, norm.set=NULL, nmean=0, nsd=1)
-{
+normalize.zscore <- function(data, norm.set=NULL, nmean=0, nsd=1){
   data = data.frame(data)
-  nums = unlist(lapply(data, is.numeric))
-  data = data[ , nums]
-
   if(is.null(norm.set))
   {
-    zscore <- matrix(nrow = 4, ncol=ncol(data))
-    zscore[1,] = t(sapply(data, mean, na.rm=TRUE))
-    zscore[2,] = t(sapply(data, sd, na.rm=TRUE))
-    zscore[3,] = t(rep(nmean, ncol(data)))
-    zscore[4,] = t(rep(nsd, ncol(data)))
-    zscore = data.frame(zscore)
+    zscore = data.frame(t(ifelse(sapply(data, is.numeric), 1, 0)))
+    zscore = rbind(zscore, rep(NA, ncol(zscore)))
+    zscore = rbind(zscore, rep(NA, ncol(zscore)))
+    zscore = rbind(zscore, rep(NA, ncol(zscore)))
+    zscore = rbind(zscore, rep(NA, ncol(zscore)))
     colnames(zscore) = colnames(data)    
-    rownames(zscore) = c("mean", "sd","nmean", "nsd")
+    rownames(zscore) = c("numeric", "mean", "sd","nmean", "nsd")
+    for (j in colnames(zscore)[zscore["numeric",]==1]) {
+      zscore["mean",j] <- mean(data[,j], na.rm=TRUE)
+      zscore["sd",j] <- sd(data[,j], na.rm=TRUE)
+      zscore["nmean",j] <- nmean
+      zscore["nsd",j] <- nsd
+    }
   }
-  for (i in 1:ncol(data))
-    data[,i] = ((data[,i] - zscore["mean", i]) / zscore["sd", i]) * zscore["nsd", i] + zscore["nmean", i]
+  else {
+    zscore = norm.set
+  }
+  for (j in colnames(minmax)[minmax["numeric",]==1]) {
+    if ((zscore["sd", j]) > 0) {
+      data[,j] = ((data[,j] - zscore["mean", j]) / zscore["sd", j]) * zscore["nsd", j] + zscore["nmean", j]
+    }
+    else {
+      data[,j] = zscore["nmean", j]  
+    }
+  }
   return (list(data=data, norm.set=zscore))
 }
 
