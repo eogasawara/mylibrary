43262da1ec5c017074828ec934a8dd531ca2d804
Eduardo Ogasawara <eogasawara@eic.cefet-rj.br>
2019-04-05 18:38:26 -0300

    Update myExploratory.R

--- myExploratory.R
+++ myExploratory.R
@@ -13,13 +13,12 @@ exp_table <- function (data, cnames = NULL, proj=NULL, n=6) {
   return(data)
 }
 
-exp_norm_dist <- function(value, label_x = "std. normal", label_y = "",  color)  {
-  data <- data.frame(value=value)
-  grf <- ggplot(data, aes(sample = value)) +
-    stat_qq(color=color) + theme_bw(base_size = 10) +
-    stat_qq_line(color=color)
-  grf <- grf + xlab(label_x)  
-  grf <- grf + xlab(label_y)  
+exp_norm_dist <- function(value, label_x = "", label_y = "",  color)  {
+  data <- data.frame(value = value)
+  grf <- ggplot(data, aes(sample = value)) + 
+    stat_qq(color=color) + xlab(label_x) + ylab(label_y) +
+    theme_bw(base_size = 10) +
+    stat_qq_line(color=color) 
   return (grf)
 }
 
