29b3c4cede05343b40cb51b70149128ba17f41f5
Eduardo Ogasawara <eogasawara@eic.cefet-rj.br>
2020-10-07 00:58:06 -0300

    Update s3preprocess-test.R

--- examples/s3preprocess-test.R
+++ examples/s3preprocess-test.R
@@ -8,27 +8,14 @@ load_series <- function(name) {
 
 x <- load_series("sin")
 
-general_test <- function(x) {
+dir_test <- function(x) {
   y <- outliers.boxplot(x)
-  
-  tt <- ts_train_test(x, test_size=5)
-  
-  swx <- ts_sw(tt$train, 5)
-  
-  swy <- outliers.boxplot(swx)
-  
-  ttsw <- ts_train_test(x, test_size=5, sw_size=5)
-  
-  swy <- outliers.boxplot(data.frame(ttsw$train))
-  
-  y <- ts_diff(x)
-  
-  ysw <- ts_diff(swx)
+  tt <- ts_train_test(y, test_size=5)
 }
-general_test(x)
+dir_test(x)
 
 
-preprocess_test <- function(prec, x, size) {
+sw_test <- function(prec, x, size) {
   tt <- ts_train_test(x, test_size=size, sw_size=size)
 
   prec <- ts_setup(prec, tt$train)
@@ -48,9 +35,9 @@ preprocess_test <- function(prec, x, size) {
   print(sprintf("%s i=%.3f - o=%.3f", class(prec)[1], abs(mean(input_org$x-io$input)), abs(mean(output_org$x-io$output))))
 }
 
-preprocess_test(ts_gminmax(), x, 10)
-preprocess_test(ts_gminmax_diff(), x, 10)
-preprocess_test(ts_swminmax(), x, 10)
-preprocess_test(ts_anminmax(), x, 10)
-preprocess_test(ts_animinmax(), x, 10)
-preprocess_test(ts_aneminmax(), x, 10)
+sw_test(ts_gminmax(), x, 10)
+sw_test(ts_gminmax_diff(), x, 10)
+sw_test(ts_swminmax(), x, 10)
+sw_test(ts_anminmax(), x, 10)
+sw_test(ts_animinmax(), x, 10)
+sw_test(ts_aneminmax(), x, 10)
