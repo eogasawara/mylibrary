9518a572aa3fab59c0c6db31f7d1686fa2912766
Eduardo Ogasawara <eogasawara@eic.cefet-rj.br>
2020-10-20 00:38:23 -0300

    Update

--- myFitting.R
+++ myFitting.R
@@ -7,99 +7,38 @@ loadlibrary <- function(x, repos='http://cran.fiocruz.br')
   }
 }
 
-loadlibrary("caret")
+# curvature
 
 
-# sampling
-
-# classe sample
-
-data_sample <- function(data) {
-  obj <- list(data=data)
-  attr(obj, "class") <- "data_sample"  
+curvature <- function(x, y, func) {
+  obj <- list(x = x, y = y, df=2, deriv=2)
+  attr(obj, "class") <- "curvature"
+  obj <- fit(obj, func)
   return(obj)
 }
 
-train_test <- function(obj, perc=0.8) {
-  UseMethod("train_test")
+fit <- function(obj, func) {
+  #x contains both input and output
+  UseMethod("fit")
 }
 
-train_test.default <- function(obj, x) {
+fit.default <- function(obj, func) {
   return(obj)
 }
 
-k_fold <- function(obj, k) {
-  UseMethod("k_fold")
-}
-
-k_fold.default <- function(obj, k) {
-  return(obj)
-}
-
-k_fold.data_sample <- function(obj, k) {
-  folds = list()
-  p = 1.0 / k
-  myobj <- obj
-  while (k > 1) {
-    obj = train_test(obj, p)
-    obj$data = obj$test
-    folds = append(folds, list(obj$train))
-    k = k - 1
-    p = 1.0 / k
-  }
-  folds = append(folds, list(obj$test))
-  obj <- myobj
-  obj$folds <- folds
-  return (obj)
-}
-
-
-
-# classe sample_random
-
-sample_random <- function(data) {
-  obj <- data_sample(data)
-  class(obj) <- append("sample_random", class(obj))  
-  return(obj)
-}
-
-train_test.sample_random <- function(obj, perc=0.8) {
-  idx = base::sample(1:nrow(obj$data),as.integer(perc*nrow(obj$data)))
-  obj$train = obj$data[idx,]
-  obj$test = obj$data[-idx,]
-  return (obj)
-}
-
-# classe sample_random
-
-sample_stratified <- function(data, label) {
-  obj <- data_sample(data)
-  obj$label <- label
-  class(obj) <- append("sample_stratified", class(obj))  
-  return(obj)
-}
-
-train_test.sample_stratified <- function(obj, perc=0.8) {
-  predictors_name  = setdiff(colnames(obj$data), obj$label)
+fit.curvature <- function(obj, func) {
+  smodel = smooth.spline(obj$x, obj$y, df = obj$df)
+  curvature = predict(smodel, x = obj$x, deriv = obj$deriv)
+  obj$yfit = func(curvature$y)
+  obj$xfit = match(obj$yfit, curvature$y)
   
-  predictors = obj$data[,predictors_name] 
-  predictand = obj$data[,obj$label] 
-  
-  idx = createDataPartition(predictand, p=perc, list=FALSE)  
-  obj$train = obj$data[idx,]
-  obj$test = obj$data[-idx,]
-  return (obj)
+  res = data.frame(x=obj$x[obj$xfit], y=obj$y[obj$xfit], z=obj$yfit)
+  obj$res <- res
+  return(obj)
 }
 
-data(iris)
-
-sr <- sample_random(iris)
-sr <- train_test(sr)
-sr <- k_fold(sr,4)
-
-ss <- sample_stratified(iris, "Species")
-ss <- train_test(ss)
-ss <- k_fold(ss,4)
+plot.curvature <- function(obj) {
+  plot(obj$x, obj$y,col=ifelse(obj$x==obj$xfit, "red", "black"))   
+}
 
 
-#samples <- sample.stratified(iris, "Species")
