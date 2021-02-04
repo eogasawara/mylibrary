e8af28594204e5c8ada09fd83b2bbd0430f71fb2
Eduardo Ogasawara <eogasawara@eic.cefet-rj.br>
2021-02-04 02:18:00 -0300

    Update

--- myBalance.R
+++ myBalance.R
@@ -5,18 +5,18 @@ source("https://raw.githubusercontent.com/eogasawara/mylibrary/master/mySample.R
 # class sample_stratified
 loadlibrary("caret")
 
-sample_stratified <- function(data, label) {
+sample_stratified <- function(data, attribute) {
   obj <- data_sample(data)
-  obj$label <- label
+  obj$attribute <- attribute
   class(obj) <- append("sample_stratified", class(obj))  
   return(obj)
 }
 
 train_test.sample_stratified <- function(obj, perc=0.8) {
-  predictors_name  = setdiff(colnames(obj$data), obj$label)
+  predictors_name  = setdiff(colnames(obj$data), obj$attribute)
   
   predictors = obj$data[,predictors_name] 
-  predictand = obj$data[,obj$label] 
+  predictand = obj$data[,obj$attribute] 
   
   idx = createDataPartition(predictand, p=perc, list=FALSE)  
   obj$train = obj$data[idx,]
@@ -27,55 +27,55 @@ train_test.sample_stratified <- function(obj, perc=0.8) {
 #class oversampling
 loadlibrary("DMwR")
 
-oversampling <- function(data, label) {
+oversampling <- function(data, attribute) {
   obj <- data_sample(as.data.frame(data))
-  obj$label <- label
+  obj$attribute <- attribute
   class(obj) <- append("oversampling", class(obj))    
   return(obj)
 }
 
 action.oversampling <- function(obj) {
   data <- obj$data
-  label <- obj$label
+  attribute <- obj$attribute
   
-  x <- sort((table(data[,label]))) 
-  class_formula = formula(paste(label, "  ~ ."))
-  data[,label] <- as.character(data[,label])
+  x <- sort((table(data[,attribute]))) 
+  class_formula = formula(paste(attribute, "  ~ ."))
+  data[,attribute] <- as.character(data[,attribute])
   mainclass = names(x)[length(x)]
   newdata = NULL
   for (i in 1:(length(x)-1)) {
     minorclass = names(x)[i]
-    curdata = data[data[,label]==mainclass | data[,label]==minorclass,]
+    curdata = data[data[,attribute]==mainclass | data[,attribute]==minorclass,]
     ratio <- as.integer(ceiling(x[length(x)]/x[i])*100)
-    curdata[,label] <- as.factor(curdata[,label])
+    curdata[,attribute] <- as.factor(curdata[,attribute])
     curdata <- SMOTE(class_formula, curdata, perc.over = ratio, perc.under=100)
-    curdata[,label] <- as.character(curdata[,label])
-    curdata = curdata[curdata[,label]==minorclass, ]
+    curdata[,attribute] <- as.character(curdata[,attribute])
+    curdata = curdata[curdata[,attribute]==minorclass, ]
     idx = sample(1:nrow(curdata),x[length(x)])
     curdata = curdata[idx,]
     newdata = rbind(newdata, curdata)
   }
-  curdata = data[data[,label]==mainclass,]
+  curdata = data[data[,attribute]==mainclass,]
   newdata = rbind(newdata, curdata)
-  newdata[,label] <- as.factor(newdata[,label])
+  newdata[,attribute] <- as.factor(newdata[,attribute])
   return(newdata)
 }
 
-subsampling <- function(data, label) {
+subsampling <- function(data, attribute) {
   obj <- data_sample(as.data.frame(data))
-  obj$label <- label
+  obj$attribute <- attribute
   class(obj) <- append("subsampling", class(obj))    
   return(obj)
 }
 
 action.subsampling <- function(obj) {
   data <- obj$data
-  label <- obj$label
-  x <- sort((table(data[,label]))) 
+  attribute <- obj$attribute
+  x <- sort((table(data[,attribute]))) 
   qminor = as.integer(x[1])
   newdata = NULL
   for (i in 1:length(x)) {
-    curdata = data[data[,label]==(names(x)[i]),]
+    curdata = data[data[,attribute]==(names(x)[i]),]
     idx = sample(1:nrow(curdata),qminor)
     curdata = curdata[idx,]
     newdata = rbind(newdata, curdata)
