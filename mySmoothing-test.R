17e8f8900d349293c981b11782da5fde2f6c7a8e
Eduardo Ogasawara <eogasawara@eic.cefet-rj.br>
2021-02-04 17:54:45 -0300

    Update mySmoothing-test.R

--- mySmoothing-test.R
+++ mySmoothing-test.R
@@ -1,11 +1,12 @@
 # version 1.0
 source("https://raw.githubusercontent.com/eogasawara/mylibrary/master/mySmoothing.R")
-
+source("https://raw.githubusercontent.com/eogasawara/mylibrary/master/myEntropy.R")
 
 bi <- smoothing_inter(iris$Sepal.Length, n=2)
 bi <- prepare(bi)
 sl.bi <- action(bi)
 table(sl.bi)
+
 
 bf <- smoothing_freq(iris$Sepal.Length, n=2)
 bf <- prepare(bf)
@@ -23,3 +24,6 @@ bi <- optimize(bi, do_plot=TRUE)
 bi <- prepare(bi)
 sl.bi <- action(bi)
 table(sl.bi)
+entro <- entropy(names(sl.bi),iris$Species)
+entro <- prepare(entro)
+res <- action(entro)
