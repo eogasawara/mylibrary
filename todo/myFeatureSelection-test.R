cbcd386f7e06d4967d37451eb5d0b5e7aecb295a
Eduardo Ogasawara <eogasawara@eic.cefet-rj.br>
2021-02-04 18:32:43 -0300

    Update

---
+++ todo/myFeatureSelection-test.R
@@ -0,0 +1,24 @@
+
+source("https://raw.githubusercontent.com/eogasawara/mylibrary/master/myPreprocessing.R")
+source("https://raw.githubusercontent.com/eogasawara/mylibrary/master/myFeature.R")
+data(iris)
+
+
+myfeature <- fs.lasso(iris, "Species")
+print(myfeature$features)
+
+myfeature <- fs.fss(iris, "Species")
+print(myfeature$features)
+
+myfeature <- fs.cfs(iris, "Species")
+print(myfeature$features)
+
+options(repr.plot.width=3, repr.plot.height=3)
+myfeature <- fs.ig(iris, "Species")
+print(myfeature$features)
+
+options(repr.plot.width=3, repr.plot.height=3)
+myfeature <- fs.relief(iris, "Species")
+print(myfeature$features)
+
+
