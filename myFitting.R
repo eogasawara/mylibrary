33b4f5d1e3ae74491acba931dfd8fa6b6a33a184
Eduardo Ogasawara <eogasawara@eic.cefet-rj.br>
2021-02-04 15:10:32 -0300

    Update myFitting.R

--- myFitting.R
+++ myFitting.R
@@ -3,35 +3,44 @@ source("https://raw.githubusercontent.com/eogasawara/mylibrary/master/myRelation
 
 # curvature
 
-
-curvature <- function(x, y, func) {
-  obj <- list(x = x, y = y, df=2, deriv=2, func=func)
-  attr(obj, "class") <- "curvature"
+curvature <- function(data) {
+  obj <- atr_transform(data)
+  obj$df <- 2
+  obj$deriv <- 2
+  class(obj) <- append("curvature", class(obj))    
   return(obj)
 }
 
-fit <- function(obj) {
-  #x contains both input and output
-  UseMethod("fit")
-}
-
-fit.default <- function(obj) {
-  return(obj)
-}
-
-fit.curvature <- function(obj) {
-  smodel = smooth.spline(obj$x, obj$y, df = obj$df)
+prepare.curvature <- function(obj) {
+  obj$x <- 1:length(obj$data)
+  smodel = smooth.spline(obj$x, obj$data, df = obj$df)
   curvature = predict(smodel, x = obj$x, deriv = obj$deriv)
-  obj$yfit = obj$func(curvature$y)
-  obj$xfit = match(obj$yfit, curvature$y)
-  
-  res = data.frame(x=obj$x[obj$xfit], y=obj$y[obj$xfit], z=obj$yfit)
-  obj$res <- res
+  obj$yfit = obj$func(curvature$data)
+  obj$xfit = match(obj$yfit, curvature$data)
   return(obj)
 }
 
 plot.curvature <- function(obj) {
-  plot(obj$x, obj$y,col=ifelse(obj$x==obj$xfit, "red", "black"))   
+  plot(obj$x, obj$data, col=ifelse(obj$x==obj$xfit, "red", "black"))   
+}
+
+action.curvature <- function(obj) {
+  res = data.frame(x=obj$x[obj$xfit], y=obj$data[obj$xfit], yfit=obj$yfit)
+  return(res)  
+}
+
+curvature_min <- function(data) {
+  obj <- curvature(data)
+  obj$func <- min
+  class(obj) <- append("curvature_min", class(obj))    
+  return(obj)
+}
+
+curvature_max <- function(data) {
+  obj <- curvature(data)
+  obj$func <- max
+  class(obj) <- append("curvature_max", class(obj))    
+  return(obj)
 }
 
 
