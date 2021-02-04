a4e53a994d489759e7e78b1fd0349fcb19b4a9b9
Eduardo Ogasawara <eogasawara@eic.cefet-rj.br>
2021-02-04 02:27:44 -0300

    Update myCategoricMapping-test.R

--- myCategoricMapping-test.R
+++ myCategoricMapping-test.R
@@ -1,5 +1,5 @@
 # version 1.0
-source("https://raw.githubusercontent.com/eogasawara/mylibrary/master/myPreprocessing.R")
+source("https://raw.githubusercontent.com/eogasawara/mylibrary/master/myCategoricMapping.R")
 
 
 cm <- categ_mapping(iris, "Species")
