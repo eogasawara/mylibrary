02eeb17a0e4eb0dec33ce68d7e5bb5cdbbf48acf
Eduardo Ogasawara <eogasawara@eic.cefet-rj.br>
2021-02-08 08:48:15 -0300

    Update myClustering-test.R

--- myClustering-test.R
+++ myClustering-test.R
@@ -3,11 +3,29 @@
 #source("https://raw.githubusercontent.com/eogasawara/mylibrary/master/myClusteringEvaluation.R")
 data(iris)
 
+eval <- cluster_evaluation(rep(1, nrow(iris)), iris$Species)
+eval <- prepare(eval)
+print(action(eval)$entropy)
+
+
 iris_cluster <- iris[,1:4]
+
+
+obj <- cluster_kmeans(iris_cluster, k=3)
+obj <- prepare(obj)
+clu <- action(obj)
+
+eval <- cluster_evaluation(clu, iris$Species)
+eval <- prepare(eval)
+print(action(eval)$entropy)
+
+
 obj <- cluster_kmeans(iris_cluster)
 obj <- prepare(obj)
 clu <- action(obj)
 
 eval <- cluster_evaluation(clu, iris$Species)
 eval <- prepare(eval)
-eval_res <- action(eval)
+print(action(eval)$entropy)
+
+
