ac2481d91c8f73e76dc71a69996c6c791935454c
Eduardo Ogasawara <eogasawara@eic.cefet-rj.br>
2021-02-12 01:01:48 -0300

    Update mySample.R

--- mySample.R
+++ mySample.R
@@ -4,24 +4,24 @@ source("https://raw.githubusercontent.com/eogasawara/mylibrary/master/myData.R")
 # data_sample
 
 data_sample <- function() {
-  obj_sample <- list()
-  attr(obj_sample, "class") <- "data_sample"  
-  return(obj_sample)
+  obj <- list()
+  attr(obj, "class") <- "data_sample"  
+  return(obj)
 }
 
-train_test <- function(obj_sample, obj_data, ...) {
+train_test <- function(obj, obj_data, ...) {
   UseMethod("train_test")
 }
 
-train_test.default <- function(obj_sample, obj_data) {
+train_test.default <- function(obj, obj_data) {
   return(list())
 }
 
-k_fold <- function(obj_sample, obj_data, k) {
+k_fold <- function(obj, obj_data, k) {
   UseMethod("k_fold")
 }
 
-k_fold.default <- function(obj_sample, obj_data, k) {
+k_fold.default <- function(obj, obj_data, k) {
   return(list())
 }
 
@@ -29,12 +29,12 @@ k_fold.default <- function(obj_sample, obj_data, k) {
 # sample_random
 
 sample_random <- function() {
-  obj_sample <- data_sample()
-  class(obj_sample) <- append("sample_random", class(obj_sample))  
-  return(obj_sample)
+  obj <- data_sample()
+  class(obj) <- append("sample_random", class(obj))  
+  return(obj)
 }
 
-train_test.sample_random <- function(obj_sample, obj_data, perc=0.8) {
+train_test.sample_random <- function(obj, obj_data, perc=0.8) {
   idx <- base::sample(1:nrow(obj_data$data),as.integer(perc*nrow(obj_data$data)))
   obj_train <- obj_data
   obj_train$data <- obj_data$data[idx,]
@@ -43,11 +43,11 @@ train_test.sample_random <- function(obj_sample, obj_data, perc=0.8) {
   return (list(train=obj_train, test=obj_test))
 }
 
-k_fold.sample_random <- function(obj_sample, obj_data, k) {
+k_fold.sample_random <- function(obj, obj_data, k) {
   folds <- list()
   p <- 1.0 / k
   while (k > 1) {
-    obj <- train_test(obj_sample, obj_data, p)
+    obj <- train_test(obj, obj_data, p)
     obj_data <- obj$test
     folds <- append(folds, list(obj$train))
     k = k - 1
@@ -59,17 +59,17 @@ k_fold.sample_random <- function(obj_sample, obj_data, k) {
 
 # sample_stratified
 sample_stratified <- function(attribute) {
-  obj_sample <- sample_random()
-  obj_sample$attribute <- attribute
-  class(obj_sample) <- append("sample_stratified", class(obj_sample))  
-  return(obj_sample)
+  obj <- sample_random()
+  obj$attribute <- attribute
+  class(obj) <- append("sample_stratified", class(obj))  
+  return(obj)
 }
 
-train_test.sample_stratified <- function(obj_sample, obj_data, perc=0.8) {
+train_test.sample_stratified <- function(obj, obj_data, perc=0.8) {
   loadlibrary("caret")
   
-  predictors_name <- setdiff(colnames(obj_data$data), obj_sample$attribute)
-  predictand <- obj_data$data[,obj_sample$attribute] 
+  predictors_name <- setdiff(colnames(obj_data$data), obj$attribute)
+  predictand <- obj_data$data[,obj$attribute] 
   
   idx <- createDataPartition(predictand, p=perc, list=FALSE) 
   obj_train <- obj_data
