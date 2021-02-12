161a69beb9af9ce8aa2126f37e1c5e01cea41548
Eduardo Ogasawara <eogasawara@eic.cefet-rj.br>
2021-02-12 14:07:16 -0300

    Update

--- myOutlier-test.R
+++ myOutlier-test.R
@@ -1,24 +1,19 @@
 # version 1.0
-source("https://raw.githubusercontent.com/eogasawara/mylibrary/master/myOutlier.R")
+#source("https://raw.githubusercontent.com/eogasawara/mylibrary/master/myOutlier.R")
 
 head(iris)
 nrow(iris)
 
 #outlier of a dataset
-out <- outliers()
-out <- prepare(out, iris)
-iris.out <- action(out, iris)
-table(out$idx)
-head(iris.out)
-nrow(iris.out)
+analyze_outliers <- function(obj, data) {
+  obj <- prepare(obj, iris)
+  ouliers <- action(obj, iris)
+  idx <- attr(outliers, "idx")
+  print(table(idx))
+  print(head(ouliers))
+  print(nrow(ouliers))
+}
 
-
-#outlier of an attribute
-out <- outliers()
-out <- prepare(out, iris$Sepal.Width)
-iris.out <- action(out, iris$Sepal.Width)
-table(out$idx)
-head(iris.out)
-length(iris.out)
-
+analyze_outliers(outliers(), iris)
+analyze_outliers(outliers(), iris$Sepal.Width)
 
--- myOutlier.R
+++ myOutlier.R
@@ -9,43 +9,58 @@ outliers <- function(alpha = 1.5) {
 }
 
 prepare.outliers <- function(obj, data) {
+  lq1 <- NA
+  hq3 <- NA
   if(is.matrix(data) || is.data.frame(data)) {
-    idx = rep(FALSE, nrow(data))
-    org <- nrow(data)
-    if (org >= 30) {
+    lq1 <- rep(NA, ncol(data))
+    hq3 <- rep(NA, ncol(data))
+    if (nrow(data) >= 30) {
       for (i in 1:ncol(data)) {
-        num <- is.numeric(data[,i])
-        if (num) {
+        if (is.numeric(data[,i])) {
           q <- quantile(data[,i])
           IQR <- q[4] - q[2]
-          lq1 <- q[2] - obj$alpha*IQR
-          hq3 <- q[4] + obj$alpha*IQR
-          idx = idx | (!is.na(data[,i]) & (data[,i] < lq1 | data[,i] > hq3))
+          lq1[i] <- q[2] - obj$alpha*IQR
+          hq3[i] <- q[4] + obj$alpha*IQR
         }
       }
     }
-    obj$idx <- idx
   }
   else {
-    idx <- rep(FALSE, length(data))
-    if (length(data) >= 30) {
+    if ((length(data) >= 30) && is.numeric(data)) {
       q <- quantile(data)
       IQR <- q[4] - q[2]
       lq1 <- q[2] - obj$alpha*IQR
       hq3 <- q[4] + obj$alpha*IQR
-      idx <- data < lq1 | data > hq3
     }
-    obj$idx <- idx
   } 
+  obj$lq1 <- lq1
+  obj$hq3 <- hq3
   return(obj)
 }
 
 action.outliers <- function(obj, data)
 {
+  idx <- FALSE
+  lq1 <- obj$lq1
+  hq3 <- obj$hq3
+  if (is.matrix(data) || is.data.frame(data)) {
+    idx = rep(FALSE, nrow(data))
+    for (i in 1:ncol(data)) 
+      if (!is.na(lq1[i]) && !is.na(hq3[i]))
+        idx = idx | (!is.na(data[,i]) & (data[,i] < lq1[i] | data[,i] > hq3[i]))
+  }
   if(is.matrix(data))
-    return(adjust.matrix(data[!obj$idx,]))
+    data <- adjust.matrix(data[!idx,])
   else if (is.data.frame(data))
-    return(adjust.data.frame(data[!obj$idx,]))
-  else 
-    return(data[!obj$idx])
+    data <- adjust.data.frame(data[!idx,])
+  else {
+    if (!is.na(lq1) && !is.na(hq3)) {
+      idx <- data < lq1 | data > hq3
+      data <- data[!idx]
+    }
+    else
+      idx <- rep(FALSE, length(data))
+  }
+  attr(data, "idx") <- idx
+  return(data)
 }
