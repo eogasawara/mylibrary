2d3ea9ddf46b968182635525bdd643861a4d8947
Eduardo Ogasawara <eogasawara@eic.cefet-rj.br>
2021-05-27 16:05:43 -0300

    Update

--- myGraphic-test.R
+++ myGraphic-test.R
@@ -1,133 +1,137 @@
-# version 0.9
-source("https://raw.githubusercontent.com/eogasawara/mylibrary/master/myGraphic.R")
+#source("https://raw.githubusercontent.com/eogasawara/mylibrary/master/myGraphic2.R")
 
-loadlibrary("MASS")
-data(iris)
+#library(gridExtra)
 
-head(iris[c(1:3,51:53,101:103),])
+col_set <- brewer.pal(11, 'Spectral')
+col_2 <- col_set[c(4,9)]
+col_3 <- col_set[c(4,9,11)]
+col_4 <- col_set[c(3,5,7,9)]   
 
-sum <- summary(iris$Sepal.Length)
-sum
+create_dataset <- function() {
+  data <- read.table(text = "Year Quarters Flights Delays
+                     2016 Q1 11 6
+                     2016 Q2 12 5
+                     2016 Q3 13 3
+                     2016 Q4 12 5
+                     2017 Q1 10 4
+                     2017 Q2 9 3
+                     2017 Q3 11 4
+                     2017 Q4 25 15
+                     2018 Q1 14 3
+                     2018 Q2 12 5
+                     2018 Q3 13 3
+                     2018 Q4 15 4",
+                     header = TRUE,sep = "")  
+  data$OnTime <- data$Flights - data$Delays 
+  data$Perc <- round(100 * data$Delays / data$Flights)
+  data$Quarters <- factor(data$Quarters, levels=c("Q1", "Q2", "Q3", "Q4"), ordered = TRUE)
+  return(data)
+}
+flightdata <- create_dataset()
+head(flightdata)
 
-IQR <- sum["3rd Qu."]-sum["1st Qu."]
-print(sprintf("IQR=%.1f", IQR))
+head(mtcars)
 
-plot_size(4, 3)
+if (TRUE) {
+  cor(mtcars)
+  plot.correlation(mtcars)
+}
 
-hist(iris$Sepal.Length)
+if (TRUE) {
+  series <- mtcars %>% select(wt, mpg, disp)
+  grf <- plot.scatter(series, label_x = "wt", colors=col_2)
+  plot(grf)
+}
 
-op <- par(mfrow=c(2,2))
-hist(iris$Sepal.Length)
-hist(iris$Sepal.Width)
-hist(iris$Sepal.Length)
-hist(iris$Sepal.Width)
+if (TRUE) {
+  series <- mtcars %>% select(wt, mpg, disp)
+  grf <- plot.series(series, label_x = "wt", colors=col_2)
+  plot(grf)
+}
 
-par(op)
+if (TRUE) {
+  series <- mtcars %>% select(wt, mpg, disp)
+  grf <- plot.series2nd(series, label_x = "wt", colors=col_2)
+  plot(grf)
+}
 
-loadlibrary("reshape")
-data <- melt(iris)
-head(data)
+if (TRUE) {
+  series <- mtcars %>% group_by(cyl) %>% summarize(hp = mean(hp))
+  grf <- plot.bar(series, colors=col_set[1])
+  grf <- grf + coord_flip()
+  plot(grf)
+}
 
-loadlibrary("RColorBrewer")
-col.set <- brewer.pal(11, 'Spectral')
-mycolors <- col.set[c(1,3,5,7,9)]
+if (TRUE) {
+  series <- mtcars %>% group_by(cyl) %>% summarize(hp = mean(hp), mpg = mean(mpg))
+  grf <- plot.bar(series, colors=col_2)
+  plot(grf)
+}
 
-grfA <- plot.boxplot(data, colors="white")
-plot(grfA)
+if (TRUE) {
+  series <- mtcars %>% group_by(cyl) %>% summarize(hp = mean(hp), mpg = mean(mpg))
+  grf <- plot.dotchar(series, colors=col_2)
+  grf <- grf + theme(axis.text.x = element_text(angle=90, hjust=1))
+  plot(grf)
+}
 
-loadlibrary("gridExtra")
-loadlibrary("dplyr")
-grfA <- plot.density(iris %>% dplyr::select(variable=Sepal.Length, value=Sepal.Length), label_x = "Sepal.Length", color="white")
-grfB <- plot.density(iris %>% dplyr::select(variable=Sepal.Width, value=Sepal.Width), label_x = "Sepal.Width", color="white")
-grfC <- plot.density(iris %>% dplyr::select(variable=Petal.Length, value=Petal.Length), label_x = "Petal.Length", color="white")
-grfD <- plot.density(iris %>% dplyr::select(variable=Petal.Width, value=Petal.Width), label_x = "Petal.Width", color="white")
-plot_size(8, 6)
-grid.arrange(grfA, grfB, grfC, grfD, ncol=2, nrow=2)
-plot_size(4, 3)
-
-grfA <- plot.density(iris %>% dplyr::select(variable=Species, value=Sepal.Length), label_x = "Sepal.Length", color=mycolors[c(1:3)])
-grfB <- plot.density(iris %>% dplyr::select(variable=Species, value=Sepal.Width), label_x = "Sepal.Width", color=mycolors[c(1:3)])
-grfC <- plot.density(iris %>% dplyr::select(variable=Species, value=Petal.Length), label_x = "Petal.Length", color=mycolors[c(1:3)])
-grfD <- plot.density(iris %>% dplyr::select(variable=Species, value=Petal.Width), label_x = "Petal.Width", color=mycolors[c(1:3)])
-plot_size(8, 6)
-grid.arrange(grfA, grfB, grfC, grfD, ncol=2, nrow=2)
-plot_size(4, 3)
-
-grfA <- plot.boxplot(iris %>% dplyr::select(variable=Species, value=Sepal.Length), label_x = "Sepal.Length", color=mycolors[c(1:3)])
-grfB <- plot.boxplot(iris %>% dplyr::select(variable=Species, value=Sepal.Width), label_x = "Sepal.Width", color=mycolors[c(1:3)])
-grfC <- plot.boxplot(iris %>% dplyr::select(variable=Species, value=Petal.Length), label_x = "Petal.Length", color=mycolors[c(1:3)])
-grfD <- plot.boxplot(iris %>% dplyr::select(variable=Species, value=Petal.Width), label_x = "Petal.Width", color=mycolors[c(1:3)])
-plot_size(8, 6)
-grid.arrange(grfA, grfB, grfC, grfD, ncol=2, nrow=2)
-plot_size(4, 3)
-
-
-plot(iris$Sepal.Length,iris$Sepal.Width)
-
-grf <- exp_correlation(iris[,1:4], color = mycolors[1:3]) 
-
-plot(grf)
-
-loadlibrary("WVPlots")
-grf <- exp_pair_plot(data=iris, cnames=colnames(iris)[1:4], title="Iris", colors=mycolors[1])
-
-plot_size(7, 5)
-grf
-plot_size(4, 3)
-
-grf <- exp_pair_plot(data=iris, cnames=colnames(iris)[1:4], clabel='Species', title="Iris", colors=mycolors[1:3])
-
-plot_size(8, 5)
-grf
-plot_size(4, 3)
-
-loadlibrary("GGally")
-grf <- exp_advpair_plot(data=iris, cnames=colnames(iris)[1:4], title="Iris", colors=mycolors[1])
-
-plot_size(7, 5)
-grf
-plot_size(4, 3)
-
-grf <- exp_advpair_plot(data=iris, cnames=colnames(iris)[1:4], title="Iris", clabel='Species', colors=mycolors[1:3])
-
-plot_size(8, 5)
-grf
-plot_size(4, 3)
-
-grf <- ggparcoord(data = iris, columns = c(1:4), group=5) + theme_bw(base_size = 10) + scale_color_manual(values=mycolors[1:3])
-
-plot_size(5, 3)
-plot(grf)
-plot_size(4, 3)
+if (TRUE) {
+  series <- mtcars %>% group_by(cyl) %>% summarize(hp = mean(hp), mpg = mean(mpg))
+  grf <- plot.stackedbar(series, colors=col_2)
+  grf <- grf + theme(axis.text.x = element_text(angle=90, hjust=1))
+  plot(grf)
+}
 
 
-mat <- as.matrix(iris[,1:4])
-x <- (1:nrow(mat))
-y <- (1:ncol(mat))
-image(x, y, mat, col = col.set, axes = FALSE,  main = "Iris", xlab="sample", ylab="Attributes")
-axis(2, at = seq(0, ncol(mat), by = 1))
-axis(1, at = seq(0, nrow(mat), by = 10))
+if (TRUE) {
+  series <- mtcars %>% group_by(cyl) %>% summarize(hp = mean(hp))
+  grf <- plot.radar(series, colors=col_set[1])
+  plot(grf)
+}
 
-set.seed(1)
-sample_rows = sample(1:nrow(iris), 25)
+if (TRUE) {
+  series <- mtcars %>% group_by(cyl) %>% summarize(hp = mean(hp))
+  grf <- plot_lollipop(series, colors=col_set[1])
+  plot(grf)
+}
 
-isample = iris[sample_rows,]
-labels = as.character(rownames(isample))
-isample$Species <- NULL
+if (TRUE) {
+  series <- mtcars %>% group_by(cyl) %>% summarize(hp = mean(hp))
+  grf <- plot.pieplot(series, colors=col_set[1:nrow(series)])
+  plot(grf)
+}
 
 
-loadlibrary("aplpack")
-plot_size(8, 6)
-faces(isample, labels = labels, print.info=F, cex=1)
-plot_size(4, 3)
+if (TRUE) {
+  grf <- plot.pieplot(flightdata %>% group_by(Quarters) %>% summarize(Flights=mean(Flights), colors=col_4)
+                      plot(grf)
+}
 
-set.seed(1)
-sample_rows = sample(1:nrow(iris), 25)
-
-isample = iris[sample_rows,]
-labels = as.character(isample$Species)
-isample$Species <- NULL
-
-plot_size(8, 6)
-faces(isample, labels = labels, print.info=F, cex=1)
-plot_size(4, 3)
+if (TRUE) {
+  loadlibrary("gridExtra")  
+  stdata <- data.frame(exponential = rexp(100000, rate = 1), 
+                       uniform = runif(100000, min = 2, max = 3), 
+                       normal = rnorm(100000, mean=5), 
+                       poisson = rpois(100000, lambda = 2))
+  
+  grfe <- plot.hist(stdata %>% select(exponential), label_x = "exponential", color=col_4[1])  
+  grfu <- plot.hist(stdata %>% select(uniform), label_x = "uniform", color=col_4[1])  
+  grfn <- plot.hist(stdata %>% select(normal), label_x = "normal", color=col_4[1])  
+  grfp <- plot.hist(stdata %>% select(poisson), label_x = "poisson", color=col_4[1])  
+  grid.arrange(grfe, grfu, grfn, grfp,ncol=2)
+  
+  grfe <- plot.density(stdata %>% select(exponential), label_x = "exponential", color=col_3[1])  
+  grfu <- plot.density(stdata %>% select(uniform), label_x = "uniform", color=col_3[2])  
+  grfn <- plot.density(stdata %>% select(normal), label_x = "normal", color=col_3[3])  
+  grid.arrange(grfe, grfu, grfn, ncol=3)  
+  
+  grf <- plot.density(stdata %>% select(exponential, uniform, normal), colors=col_3)
+  plot(grf)
+  
+  grf <- plot.boxplot(stdata, colors=col_4)
+  plot(grf)  
+  
+  
+  grf <- plot.norm_dist(rexp(1000, rate=1), colors=col_3[1])
+  plot(grf)
+}
