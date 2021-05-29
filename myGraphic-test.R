2c6fb7a65d72f835ddd5b6d7a6bbc87c9a91ca46
Eduardo Ogasawara <eogasawara@eic.cefet-rj.br>
2021-05-29 13:21:54 -0300

    Update

--- myGraphic-test.R
+++ myGraphic-test.R
@@ -1,108 +1,87 @@
 suppressPackageStartupMessages(source("https://raw.githubusercontent.com/eogasawara/mylibrary/master/myGraphic.R"))
 
 col_set <- brewer.pal(11, 'Spectral')
-colors <- col_set[c(3,5,7,9)]   
+colors <- col_set[c(3,5,7,9)]
 
 # This function is used only to set graphics size in this notebook. Ignore it for the moment.
 plot.size(10, 5)
 
-create_dataset <- function() {
-  data <- read.table(text = "Year Quarters Flights Delays
-                     2016 Q1 11 6
-                     2016 Q2 12 5
-                     2016 Q3 13 3
-                     2016 Q4 12 5
-                     2017 Q1 10 4
-                     2017 Q2 9 3
-                     2017 Q3 11 4
-                     2017 Q4 25 15
-                     2018 Q1 14 3
-                     2018 Q2 12 5
-                     2018 Q3 13 3
-                     2018 Q4 15 4",
-                     header = TRUE,sep = "")  
-  data$OnTime <- data$Flights - data$Delays 
-  data$Perc <- round(100 * data$Delays / data$Flights)
-  data$Quarters <- factor(data$Quarters, levels=c("Q1", "Q2", "Q3", "Q4"), ordered = TRUE)
-  return(data)
-}
-flightdata <- create_dataset()
-head(flightdata)
+# setting the font size for charts
+font <- theme(text = element_text(size=16))
 
 head(mtcars)
 
+# The dataset to be plotted   
 series <- mtcars %>% select(wt, mpg, disp)
 head(series)
 
+# The function returns a preset graphic that can be enhanced. 
 grf <- plot.scatter(series, label_x = "wt", colors=colors[1:2])
-grf <- grf + theme(text = element_text(size=16))
+# Increasing the font size of the graphics
+grf <- grf + font
+# Actual plot
 plot(grf)
 
-grf <- plot.series(series, label_x = "wt", colors=colors[1:2])
-grf <- grf + theme(text = element_text(size=16))
+grf <- plot.series(series, label_x = "wt", colors=colors[1:2]) + font
 plot(grf)
 
 series <- mtcars %>% select(wt, mpg, disp)
-grf <- plot.series2nd(series, label_x = "wt", colors=colors[1:2])
-grf <- grf + theme(text = element_text(size=16))
+grf <- plot.series2nd(series, label_x = "wt", colors=colors[1:2]) + font
 plot(grf)
 
+# The dataset to be plotted  
 series <- mtcars %>% group_by(cyl) %>% summarize(hp = mean(hp))
 head(series)
 
-grf <- plot.bar(series, colors=col_set[1])
-grf <- grf + theme(text = element_text(size=16))
+grf <- plot.bar(series, colors=col_set[1]) + font
 plot(grf)
 
+# Sometimes the bars can be plotted vertically. Use function coord_flip() for that.
 grf <- grf + coord_flip()
 plot(grf)
 
-data_sd <- flightdata %>% 
-  select(variable=Quarters, value=Delays) %>% 
-  group_by(variable) %>% 
-  summarize(mean = mean(value), sd = sd(value))
-head(data_sd)
+grf <- plot.lollipop(series, colors=col_set[1]) + font
+plot(grf)
 
-grf <- plot.bar(data_sd, colors=colors[1], alpha=0.5)
-grf <- grf + geom_errorbar(aes(x=variable, ymin=mean-sd, ymax=mean+sd), width=0.2, colour=colors[2], alpha=0.9, size=1.1) 
-grf <- grf + theme(text = element_text(size=16))
+series <- mtcars %>% group_by(cyl) %>% summarize(mean = mean(hp), sd=sd(hp))
+head(series)
+
+grf <- plot.bar(series, colors=colors[1], alpha=0.5) + font
+grf <- grf + geom_errorbar(aes(x=cyl, ymin=mean-sd, ymax=mean+sd), width=0.2, colour=colors[2], alpha=0.9, size=1.1) 
 plot(grf)
 
 series <- mtcars %>% group_by(cyl) %>% summarize(hp = mean(hp), mpg = mean(mpg))
 head(series)
 
-grf <- plot.groupedbar(series, colors=colors[1:2])
-grf <- grf + theme(text = element_text(size=16))
+grf <- plot.groupedbar(series, colors=colors[1:2]) + font
 plot(grf)
 
-grf <- plot.stackedbar(series, colors=colors[1:2])
+grf <- plot.stackedbar(series, colors=colors[1:2]) + font
 grf <- grf + theme(axis.text.x = element_text(angle=90, hjust=1))
-grf <- grf + theme(text = element_text(size=16))
 plot(grf)
 
-grf <- plot.pieplot(flightdata %>% group_by(Quarters) %>% summarize(Flights=mean(Flights)), colors=colors[1:4])
-grf <- grf + theme(text = element_text(size=16))
+grf <- plot.dotchar(series, colors=colors[1:2]) + font
+grf <- grf + theme(axis.text.x = element_text(angle=90, hjust=1))
+plot(grf)
+
+# Dataset used
+series <- mtcars %>% group_by(cyl) %>% summarize(hp = mean(hp))
+head(series)
+
+grf <- plot.pieplot(series, colors=colors[1:nrow(series)]) + font
 plot(grf)
 
 series <- mtcars %>% group_by(cyl) %>% summarize(hp = mean(hp))
 head(series)
 
-grf <- plot.radar(series, colors=colors[1])
+grf <- plot.radar(series, colors=colors[1]) + font
 grf <- grf + ylim(0, NA)
-grf <- grf + theme(text = element_text(size=16))
-plot(grf)
-
-grf <- plot.lollipop(series, colors=col_set[1])
-grf <- grf + theme(text = element_text(size=16))
 plot(grf)
 
 series <- mtcars %>% group_by(cyl) %>% summarize(hp = mean(hp), mpg = mean(mpg))
 head(series)
 
-grf <- plot.dotchar(series, colors=colors[1:2])
-grf <- grf + theme(axis.text.x = element_text(angle=90, hjust=1))
-plot(grf)
-
+# Correlation matrix
 cor(mtcars)
 
 plot.correlation(mtcars)
@@ -113,25 +92,25 @@ stdata <- data.frame(exponential = rexp(100000, rate = 1),
                      poisson = rpois(100000, lambda = 2))
 head(stdata)
 
-grfe <- plot.hist(stdata %>% select(exponential), label_x = "exponential", color=colors[1])  
-grfu <- plot.hist(stdata %>% select(uniform), label_x = "uniform", color=colors[1])  
-grfn <- plot.hist(stdata %>% select(normal), label_x = "normal", color=colors[1])  
-grfp <- plot.hist(stdata %>% select(poisson), label_x = "poisson", color=colors[1])  
+grfe <- plot.hist(stdata %>% select(exponential), label_x = "exponential", color=colors[1]) + font
+grfu <- plot.hist(stdata %>% select(uniform), label_x = "uniform", color=colors[1]) + font  
+grfn <- plot.hist(stdata %>% select(normal), label_x = "normal", color=colors[1]) + font 
+grfp <- plot.hist(stdata %>% select(poisson), label_x = "poisson", color=colors[1]) + font
 loadlibrary("gridExtra") 
 grid.arrange(grfe, grfu, grfn, grfp,ncol=2)
 
-grfe <- plot.density(stdata %>% select(exponential), label_x = "exponential", color=colors[1])  
-grfu <- plot.density(stdata %>% select(uniform), label_x = "uniform", color=colors[2])  
-grfn <- plot.density(stdata %>% select(normal), label_x = "normal", color=colors[3])  
+grfe <- plot.density(stdata %>% select(exponential), label_x = "exponential", color=colors[1]) + font  
+grfu <- plot.density(stdata %>% select(uniform), label_x = "uniform", color=colors[2]) + font  
+grfn <- plot.density(stdata %>% select(normal), label_x = "normal", color=colors[3]) + font  
 grid.arrange(grfe, grfu, grfn, ncol=3) 
 
-grf <- plot.density(stdata %>% select(exponential, uniform, normal), colors=colors[1:3])
+grf <- plot.density(stdata %>% select(exponential, uniform, normal), colors=colors[1:3]) + font
 plot(grf)
 
-grf <- plot.boxplot(stdata, colors=colors[1:4])
+grf <- plot.boxplot(stdata, colors=colors[1:4]) + font
 plot(grf)  
 
-grf <- plot.norm_dist(rexp(1000, rate=1), colors=colors[1])
+grf <- plot.norm_dist(rexp(1000, rate=1), colors=col_3[1]) + font
 plot(grf)
 
 pdf("examples/plot.pdf", width=4, height=3)
