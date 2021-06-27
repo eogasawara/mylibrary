0eea042077140b2de937e7a5c8c24ceed45855e2
Eduardo Ogasawara <eogasawara@eic.cefet-rj.br>
2021-06-27 20:19:23 -0300

    Update myTSRegression-test.R

--- myTSRegression-test.R
+++ myTSRegression-test.R
@@ -1,5 +1,5 @@
 # version 1.1
-#source("https://raw.githubusercontent.com/eogasawara/mylibrary/master/myTSRegression.R")
+source("https://raw.githubusercontent.com/eogasawara/mylibrary/master/myTSRegression.R")
 
 load_series <- function(name) {
   link <- url(sprintf("https://raw.githubusercontent.com/eogasawara/mylibrary/master/data/time-series/%s.RData", name))
