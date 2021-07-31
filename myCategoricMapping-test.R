57dab8a1c673f0ce8a633ed157e3889fbb2ee77e
Eduardo Ogasawara <eogasawara@eic.cefet-rj.br>
2021-07-31 11:49:02 -0300

    Update

--- myCategoricMapping-test.R
+++ myCategoricMapping-test.R
@@ -2,6 +2,7 @@
 source("https://raw.githubusercontent.com/eogasawara/mylibrary/master/myTransform.R")
 source("https://raw.githubusercontent.com/eogasawara/mylibrary/master/myCategoricMapping.R")
 
+iris <- datasets::iris
 cm <- categ_mapping("Species")
 iris_cm <- action(cm, iris)
 print(head(iris_cm))
