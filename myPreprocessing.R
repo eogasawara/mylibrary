578f0e1f8d0c19c98fa4273313cb9c682bfc1400
Eduardo Ogasawara <eogasawara@eic.cefet-rj.br>
2021-07-31 21:16:58 -0300

    Update

--- myPreprocessing.R
+++ myPreprocessing.R
@@ -235,3 +235,31 @@ prepare.smoothing_cluster <- function(obj, data) {
   return(obj)
 }
 
+smoothing_evaluation <- function(data, attribute) {
+  obj <- list(data=as.factor(data), attribute=as.factor(attribute))
+  attr(obj, "class") <- "cluster_evaluation"  
+  
+  library(dplyr)
+  
+  compute_entropy <- function(obj) {
+    value <- getOption("dplyr.summarise.inform")
+    options(dplyr.summarise.inform = FALSE)
+    
+    base <- data.frame(x = obj$data, y = obj$attribute) 
+    tbl <- base %>% group_by(x, y) %>% summarise(qtd=n()) 
+    tbs <- base %>% group_by(x) %>% summarise(t=n()) 
+    tbl <- merge(x=tbl, y=tbs, by.x="x", by.y="x")
+    tbl$e <- -(tbl$qtd/tbl$t)*log(tbl$qtd/tbl$t,2)
+    tbl <- tbl %>% group_by(x) %>% summarise(ce=sum(e), qtd=sum(qtd)) 
+    tbl$ceg <- tbl$ce*tbl$qtd/length(obj$data)
+    obj$entropy_clusters <- tbl
+    obj$entropy <- sum(obj$entropy$ceg)
+    
+    options(dplyr.summarise.inform = value)
+    return(obj)
+  }
+  obj <- compute_entropy(obj)
+  return(obj)
+}
+
+
